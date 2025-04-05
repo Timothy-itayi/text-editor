e90acce32922749b84404703a6b0d69e94986876
Timothy itayi <timmytam10@gmail.com>
2025-04-05 15:11:52 +1100

    clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+/* includes  */
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -6,9 +8,19 @@
 #include <unistd.h>
 
 
+/** defines  */
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+/** data */
 struct termios orig_termios;
 
+
+/** terminal */
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J]", 4);
+    wirte(STDOUT_FILENO, "\x1b[H", 3);
+
+
     perror(s);
     exit(1);
 }
@@ -18,6 +30,7 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
+
    if (tcgetattr(STDIN_FILENO, &orig_termios)== -1) die("tecgetattr");
     atexit(disableRawMode);
 
@@ -32,18 +45,45 @@ void enableRawMode() {
 
    if( tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+char editorReadKey() {
+    int nread;
+    char c; 
+    while((nread = read(STDERR_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+
+/** output  */
+
+void editorRefreshScreen() {
+
+    write(STDOUT_FILENO, "\x1b[2J]", 4);
+    wirte(STDOUT_FILENO, "\x1b[H", 3);
+
+}
+/** input */
+void editorProcessKEypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J]", 4);
+        wirte(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
+    }
+}
+/** init */
 int main() {
+
 enableRawMode();
 
     while(1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n",c, c);
-        }
-        if (c == 'q') break;
+        editorRefreshScreen();
+        editorProcessKEypress();
     }
     return 0;
 }
